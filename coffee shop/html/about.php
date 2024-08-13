40014acf383d11e5d51873fca231de51a87e62f6
Nishanthini04 <146074345+Nishanthini04@users.noreply.github.com>
2024-08-13 03:00:39 -0700

    Add files via upload

---
+++ coffee shop/html/about.php
@@ -0,0 +1,27 @@
+<?php include("index.html")
+?>
+<head>
+     <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+     <div id="ABOUT">
+        <div class="row3">
+
+          <div class="col5">
+                <img src="images/about bg.jpg" height="350px" width="500px" alt="about page image">
+          </div>
+     <div class="col6">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+
+     </div>
+     </div>
+          <div class="row4">
+            <marquee direction="left">
+                 <p> Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            </marquee>
+                 <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p>
+         </div>
+    </div>
+</body>
